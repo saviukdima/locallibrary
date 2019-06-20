237101d3d043caa2a393272f403c23588d14d42b
savyukdima <savyuk.dima@gmail.com>
2019-06-20 20:21:43 +0300

    creating home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+	num_books = Book.objects.all().count()
+	num_instances = BookInstance.objects.all().count()
+	num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+	num_authors = Author.objects.all().count()
+
+	num_genres = Genre.objects.all().count()
+	num_books_with_kill = Book.objects.filter(title__contains='Kill').count()
+
+	context = {
+		'num_books': num_books,
+		'num_instances': num_instances,
+		'num_instances_available':num_instances_available,
+		'num_authors': num_authors,
+		'num_genres': num_genres,
+		'num_books_with_kill': num_books_with_kill,
+	}
+
+	return render(request, 'index.html', context=context)
--- localibrary/settings.py
+++ localibrary/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'localibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'template')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
