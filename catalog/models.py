9b6759b92e103f07f4be73a9409e70404e805220
savyukdima <savyuk.dima@gmail.com>
2019-06-21 18:26:58 +0300

    generic list and detail views

--- catalog/models.py
+++ catalog/models.py
@@ -21,6 +21,7 @@ class Genre(models.Model):
 
 class Book(models.Model):
 	title = models.CharField(max_length=200)
+	summary = models.TextField()
 	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 	genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
@@ -35,7 +36,7 @@ class Book(models.Model):
 		ordering = ['title']
 
 	def display_genre(self):
-		return ', '.join(genre.name for genre in self.genre.all()[:3])
+		return ', '.join(genre.name for genre in self.genre.all())
 
 	display_genre.short_description = 'Genre'
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
 def index(request):
@@ -21,3 +22,16 @@ def index(request):
 	}
 
 	return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+	model = Book
+	paginate_by = 3
+
+class BookDetailView(generic.DetailView):
+	model = Book
+
+class AuthorListView(generic.ListView):
+	model = Author
+
+class AuthorDetailView(generic.DetailView):
+	model = Author
